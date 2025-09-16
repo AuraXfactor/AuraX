3c59fbdafeb0e77953295608e72060ba9d591a49
AuraXfactor <auraxfactor@gmail.com>
2025-09-16 03:19:18 +0300

    Update page.tsx
    Login page.tsx update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -72,7 +72,7 @@ export default function Login() {
         </button>
 
         <p className="text-center text-sm">
-          Don't have an account?{' '}
+          Don&apos;t have an account?
           <Link href="/signup" className="text-blue-500">
             Sign up
           </Link>
